e1a6f9df882341073f1b62bd5c574eda107eae3a
xiaolong.jin <xiaolong.jin@icloud.com>
2020-06-12 14:08:57 +0800

    update

--- SwiftWithOC/ViewController.swift
+++ SwiftWithOC/ViewController.swift
@@ -9,29 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         let _ = cellForRow()
     }
-
+    
     let tableView = OCTableView()
-
+    
     func cellForRow() -> UITableViewCell {
         var cell: OCTableViewCell! = (tableView.cellMayReturnNil() as! OCTableViewCell)
         
         if cell == nil {
+            switch cell {
+            case .none:
+                print("cell is nil")
+            case .some(let v):
+                if v == nil {
+                    print("v == nil")
+                } else {
+                    print("v != nil")
+                }
+            }
+            
             cell = OCTableViewCell()
             // init
             print("cell is nil")
         } else {
             print("cell is not nil")
+            
+            print("cell = \(String(describing: cell))")
+            print("cell = \(type(of: cell))")
+            
+            switch cell {
+            case .none:
+                print("cell is nil")
+            case .some(let v):
+                if v == nil {
+                    print("v == nil")
+                } else {
+                    print("v != nil")
+                }
+            }
         }
         return cell
     }
-
-
+    
+    
 }
 
